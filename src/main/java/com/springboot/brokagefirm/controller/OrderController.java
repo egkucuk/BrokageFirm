cf9325879d7733b04ea3595e1aaf4a0925a4871a
egkucuk <ezgiguzelkucuk@gmail.com>
2024-09-30 15:05:59 +0300

    brokage firm

--- src/main/java/com/springboot/brokagefirm/controller/OrderController.java
+++ src/main/java/com/springboot/brokagefirm/controller/OrderController.java
@@ -14,7 +14,7 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 @RestController
-@RequestMapping("api/brokagefirm")
+@RequestMapping("/api/brokagefirm")
 public class OrderController {
 
     @Autowired
