31163d05f3b7a842950bcb6e4a3d54cf5e97666d
egkucuk <ezgiguzelkucuk@gmail.com>
2024-09-30 19:36:50 +0300

    brokage firm

---
+++ src/main/java/com/springboot/brokagefirm/config/SeurityConfig.java
@@ -0,0 +1,41 @@
+package com.springboot.brokagefirm.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.web.util.matcher.AntPathRequestMatcher.antMatcher;
+
+@EnableWebSecurity
+@Configuration
+public class SeurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.csrf(csfr -> csfr.disable())
+                .authorizeHttpRequests(authorize -> {
+                    authorize.requestMatchers(antMatcher("/h2-console/**")).permitAll()
+                            .requestMatchers(antMatcher(HttpMethod.GET, "/api/brokagefirm/orders/**")).permitAll()
+                            .requestMatchers(antMatcher(HttpMethod.POST, "/api/brokagefirm/orders")).permitAll()
+                            .anyRequest().authenticated();
+                })
+                .headers(h -> h.frameOptions(HeadersConfigurer.FrameOptionsConfig::sameOrigin)) // This so embedded frames in h2-console are working
+                .httpBasic(Customizer.withDefaults())
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
+        return httpSecurity.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
